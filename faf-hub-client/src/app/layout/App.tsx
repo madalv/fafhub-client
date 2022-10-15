591279b96a91ece30218652e099b7d660b2f02e0
madalv <vlada.magal@isa.utm.md>
2022-10-15 20:53:57 +0300

    added basic room store w mobx

--- faf-hub-client/src/app/layout/App.tsx
+++ faf-hub-client/src/app/layout/App.tsx
@@ -1,26 +1,20 @@
-import React, {useEffect, useState} from "react";
-import {Room} from "../models/Room";
+import React, {useEffect} from "react";
 import {List} from "semantic-ui-react";
 import {useStore} from "../stores/store";
 import {observer} from "mobx-react-lite";
-import { agent } from "../api/agent"
 
 
 const App: React.FC = () => {
     const {roomStore} = useStore()
-    const [rooms, setRooms] = useState<Room[]>([])
 
     useEffect(() => {
-        agent.Rooms.list()
-            .then((response) => {
-                setRooms(response)
-            })
-    }, [])
+        roomStore.loadRooms()
+    }, [roomStore])
 
     return (
         <>
             <List>
-                {rooms.map((room) => (
+                {roomStore.rooms.map((room) => (
                     <List.Item key={room.id}>
                         {room.name}: {room.ownerId}
                     </List.Item>
--- faf-hub-client/src/app/stores/roomStore.ts
+++ faf-hub-client/src/app/stores/roomStore.ts
@@ -1,18 +1,25 @@
 import {makeAutoObservable} from "mobx";
 import {Room} from "../models/Room";
+import {agent} from "../api/agent";
 
 export default class RoomStore {
     rooms: Room[] = []
     selectedRoom: Room | null = null
-    loadingInitial = false
 
     constructor() {
         makeAutoObservable(this)
     }
 
-    loadActivities = () => {
-        this.loadingInitial = true
-
+    loadRooms = async () => {
+        try {
+            const rooms = await agent.Rooms.list()
+            this.setRooms(rooms)
+        } catch (error) {
+            console.log(error)
+        }
     }
 
+    setRooms = (rooms: Room[]) => {
+        this.rooms = rooms
+    }
 }
